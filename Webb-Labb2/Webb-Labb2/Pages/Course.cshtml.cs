2f6e4daf0e389c088ed75202a14a46dd43201073
ErikVerta <89127095+ErikVerta@users.noreply.github.com>
2022-03-23 18:50:47 +0100

    added razor pages to project

---
+++ Webb-Labb2/Webb-Labb2/Pages/Course.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Webb_Labb2.Pages
+{
+    public class CourseModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- Webb-Labb2/Webb-Labb2/Program.cs
+++ Webb-Labb2/Webb-Labb2/Program.cs
@@ -15,25 +15,22 @@ builder.Services.AddScoped<CourseStorage>();
 builder.Services.AddScoped<UserStorage>();
 builder.Services.AddScoped<DifficultyStorage>();
 builder.Services.AddScoped<UserCourseStorage>();
+builder.Services.AddRazorPages();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapRazorPages();
+
 
 app.Run();
